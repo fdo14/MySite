8bb11925160360bbf617364a7d2a4ad51c18b4bb
Jack <jack.nichols404@gmail.com>
2018-12-06 17:55:48 -0500

    Going Mobile

--- www/script.js
+++ www/script.js
@@ -1,34 +1,150 @@
-var x = document.getElementById("modules").querySelectorAll(".col-sm-4");
+function clickRight(){
+  var half = $("#center").parent().width() / 2 - $("#left").width() / 2;
+  var negHalf = half * (-1);
+  $("#right").animate({
+      left: negHalf
+  }, 500);
+  $("#center").animate({
+      left: negHalf
+  }, 500);
 
-var id = 1;
+  $("#left").animate({left: negHalf
+  }, 500);
+
+  var nodes = document.getElementById('right').childNodes;
+  for(var i=0; i<nodes.length; i++) {
+    if (nodes[i].nodeName.toLowerCase() == 'div' && nodes[i].classList.contains("hidden")) {
+        nodes[i].classList.remove("hidden");
+     } else if (nodes[i].nodeName.toLowerCase() == 'div' && nodes[i].classList.contains("innerCenter")) {
+         nodes[i].classList.remove("offCenter");
+      }
+   }
+
+   var nodesCenter = document.getElementById('center').childNodes;
+   for(var i=0; i<nodesCenter.length; i++) {
+     if (nodesCenter[i].nodeName.toLowerCase() == 'div' && (nodesCenter[i].classList.contains("innerLeft") || nodesCenter[i].classList.contains("innerRight"))) {
+         $(nodesCenter[i]).addClass('hidden', 1000);
+      } else if (nodesCenter[i].nodeName.toLowerCase() == 'div' && nodesCenter[i].classList.contains("innerCenter")) {
+          $(nodesCenter[i]).addClass('offCenter');
+       }
+    }
+    document.getElementById('center').setAttribute('onclick', 'clickLeftToHome()');
+}
+
+function clickRightToHome(){
+  $("#center").animate({
+      'left': '0'
+  }, 500);
+  $("#left").animate({
+      'left': '0'
+  }, 500);
+  $("#right").animate({
+      'left': '0'
+  }, 500);
+
+  var nodesCenter = document.getElementById('center').childNodes;
+  for(var i=0; i<nodesCenter.length; i++) {
+    if (nodesCenter[i].nodeName.toLowerCase() == 'div' && (nodesCenter[i].classList.contains("innerLeft") || nodesCenter[i].classList.contains("innerRight"))) {
+        $(nodesCenter[i]).removeClass('hidden', 1000);
+     } else if (nodesCenter[i].nodeName.toLowerCase() == 'div' && nodesCenter[i].classList.contains("innerCenter")) {
+         $(nodesCenter[i]).removeClass('offCenter');
+      }
+   }
+
+   var nodes = document.getElementById('left').childNodes;
+   for(var i=0; i<nodes.length; i++) {
+     if (nodes[i].nodeName.toLowerCase() == 'div' && (nodes[i].classList.contains("innerLeft") || nodes[i].classList.contains("innerRight"))) {
+         $(nodes[i]).addClass('hidden', 1000);
+      } else if (nodes[i].nodeName.toLowerCase() == 'div' && nodes[i].classList.contains("innerCenter")) {
+          $(nodes[i]).addClass('offCenter');
+       }
+    }
+}
+
+function clickLeftToHome(){
+  $("#center").animate({
+      'left': '0'
+  }, 500);
+  $("#left").animate({
+      'left': '0'
+  }, 500);
+  $("#right").animate({
+      'left': '0'
+  }, 500);
+
+  var nodesCenter = document.getElementById('center').childNodes;
+  for(var i=0; i<nodesCenter.length; i++) {
+    if (nodesCenter[i].nodeName.toLowerCase() == 'div' && (nodesCenter[i].classList.contains("innerLeft") || nodesCenter[i].classList.contains("innerRight"))) {
+        $(nodesCenter[i]).removeClass('hidden', 1000);
+     } else if (nodesCenter[i].nodeName.toLowerCase() == 'div' && nodesCenter[i].classList.contains("innerCenter")) {
+         $(nodesCenter[i]).removeClass('offCenter');
+      }
+   }
+
+   var nodes = document.getElementById('right').childNodes;
+   for(var i=0; i<nodes.length; i++) {
+     if (nodes[i].nodeName.toLowerCase() == 'div' && (nodes[i].classList.contains("innerLeft") || nodes[i].classList.contains("innerRight"))) {
+         $(nodes[i]).addClass('hidden', 1000);
+      } else if (nodes[i].nodeName.toLowerCase() == 'div' && nodes[i].classList.contains("innerCenter")) {
+          $(nodes[i]).addClass('offCenter');
+       }
+    }
+
+}
 
 function clickLeft(){
-  x[id-2].style.display = "block";
-  x[id+1].style.display = "none";
-  id--;
+  $("#right").animate({
+      left: $("#center").parent().width() / 2 - $("#left").width() / 2
+  }, 500);
+  $("#center").animate({
+      left: $("#center").parent().width() / 2 - $("#left").width() / 2
+  }, 500);
+
+  $("#left").animate({left: $("#center").parent().width() / 2 - $("#left").width() / 2
+  }, 500);
+
+  var nodes = document.getElementById('left').childNodes;
+  for(var i=0; i<nodes.length; i++) {
+    if (nodes[i].nodeName.toLowerCase() == 'div' && nodes[i].classList.contains("hidden")) {
+        nodes[i].classList.remove("hidden");
+     } else if (nodes[i].nodeName.toLowerCase() == 'div' && nodes[i].classList.contains("innerCenter")) {
+         nodes[i].classList.remove("offCenter");
+      }
+   }
+
+   var nodesCenter = document.getElementById('center').childNodes;
+   for(var i=0; i<nodesCenter.length; i++) {
+     if (nodesCenter[i].nodeName.toLowerCase() == 'div' && (nodesCenter[i].classList.contains("innerLeft") || nodesCenter[i].classList.contains("innerRight"))) {
+         $(nodesCenter[i]).addClass('hidden', 1000);
+      }
+      else if (nodesCenter[i].nodeName.toLowerCase() == 'div' && nodesCenter[i].classList.contains("innerCenter")) {
+          $(nodesCenter[i]).addClass('offCenter');
+       }
+    }
+
+    document.getElementById('center').setAttribute('onclick', 'clickRightToHome()');
+
+
+
+
 }
 
-function clickRight(){
-  x[id-1].style.display = "none";
-  x[id+2].style.visibility = "visible";
-  id++;
+$("#cool").click(function () {
+  $(this).animate({
+      left: $(this).parent().width() / 2 - $(this).width() / 2
+  }, 500);
+  $("#cool1").animate({
+      left: $(this).parent().width() / 2 - $(this).width() / 2
+  }, 500);
+  $("#cool2").animate({left: "1000px"}, "fast");
+});
 
-  var idNewCenterModule = x[id].id
-  var idNewRightModule = x[id+1].id
-  var idNewLeftModule = x[id-1].id
-
-  var centerClass = document.getElementById(idNewCenterModule);
-  centerClass.classList.remove("right");
-  document.getElementById(idNewCenterModule).onclick = null;
-
-  var leftClass = document.getElementById(idNewLeftModule);
-  leftClass.classList.add("left");
-  document.getElementById(idNewLeftModule).setAttribute('onclick','clickLeft()')
-
-  var rightClass = document.getElementById(idNewRightModule);
-  rightClass.classList.add("right");
-
-
-
-
-}
+$("#cool2").click(function () {
+  $(this).animate({
+      right: $(this).parent().width() / 2 - $(this).width() / 2
+  }, 500);
+  $("#cool1").animate({
+      right: $(this).parent().width() / 2 - $(this).width() / 2
+  }, 500);
+  $("#cool").animate({right: "1000px"}, "fast");
+});
